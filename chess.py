a9ada6c8c90aa4fe64526cc61dab3c304d17f7e5
Ang Leng Khai <92260463+Zer0Legion@users.noreply.github.com>
2021-10-18 15:43:57 +0800

    Check for allowed spaces to move
    Completed the checking of allowed spaces to move (except king), not yet added the ability to move pieces
    
    - Square naming should revamp

--- chess.py
+++ chess.py
@@ -1,25 +1,27 @@
-#8     bQ bK
-#7
-#6
-#5
-#4
-#3
-#2
-#1      Q K
-# a b c d e f g h
-#x 1 2 3 4 5 6 7 8
 import math
 
-bOriPos = {'br1':36, 'bk1':58, 'bb1':59, 'bq':60, 'bk':61, 'bb2': 62, 'bk2':63, 'br2': 64 , 'bp1': 49, 'bp2': 50, 'bp3': 51, 'bp4': 52, 'bp5': 53, 'bp6': 54, 'bp7': 55, 'bp8': 56 }
-OriPos = {'r1':38, 'k1':2, 'b1':3, 'q':4, 'k':5, 'b2': 6, 'k2':7, 'r2': 8 , 'p1': 9, 'p2': 10, 'p3': 11, 'p4': 12, 'p5': 13, 'p6': 14, 'p7': 15, 'p8': 16 }
+bOriPos = {'br1':57, 'bn1':58, 'bb1':59, 'bq1':60, 'bk1':28, 'bb2': 62, 'bn2':63, 'br2': 64 , 'bp1': 49, 'bp2': 50, 'bp3': 51, 'bp4': 52, 'bp5': 53, 'bp6': 54, 'bp7': 55, 'bp8': 56 }
+OriPos = {'r1':1, 'n1':2, 'b1':3, 'q1':4, 'k1':5, 'b2': 6, 'n2':7, 'r2': 8 , 'p1': 9, 'p2': 10, 'p3': 11, 'p4': 12, 'p5': 13, 'p6': 14, 'p7': 15, 'p8': 16 }
 
 takenSquares = []
 btakenSquares = []
+threats = [] #threats for white (black avail moves)
+bthreats = [] #threats for black (white avail moves)
+
+#update the squares taken by each colour
 def updateBoardPositionsOnly():
     for i in black.items():
         btakenSquares.append(i[1])
     for i in white.items():
         takenSquares.append(i[1])
+def updatethreats(threatset, enemy): #enemy is just colour
+    threatset.clear()
+    for i in enemy.keys():
+        list = availmoves(i)
+        if len(list) != 0:
+            for i in list:
+                threatset.append(i)
+
 
 
 black = bOriPos
@@ -34,7 +36,7 @@ def rookmoves(p, ally, enemy):
     yminuscheck = getPos(p)
     while True:
         xpluscheck = xpluscheck + 1
-        print('checking xplus',xpluscheck)
+        #print('checking xplus',xpluscheck)
         if getrow(p) != getrow(xpluscheck):
             break
         if getPos(xpluscheck) in ally:
@@ -48,63 +50,200 @@ def rookmoves(p, ally, enemy):
     xminusstart = 'starting'
     while xminusstart == 'starting':
         xminuscheck = xminuscheck - 1
-        print('checking xminus',xminuscheck)
+        #print('checking xminus',xminuscheck)
         if getrow(p) != getrow(xminuscheck):
-            print('diff row')
+            #print('diff row')
             break
         if getPos(xminuscheck) in ally:
-            print('ally taken')
+            #print('ally taken')
             break
         elif getPos(xminuscheck) in enemy:
             moves.append(xminuscheck)
-            print('enemy taken')
+            #print('enemy taken')
             break
         else:
             moves.append(xminuscheck)
-            print('appending',xminuscheck)
+            #print('appending',xminuscheck)
             continue
     yplusstart = 'start'
     while yplusstart == 'start':
         ypluscheck = ypluscheck + 8
-        print('checking yplus',ypluscheck)
+        #print('checking yplus',ypluscheck)
         if ypluscheck > 64 or ypluscheck < 1:
-            print('out of area')
+            #print('out of area')
             break
         if getPos(ypluscheck) in ally:
-            print('ally taken')
+            #print('ally taken')
             break
         elif getPos(ypluscheck) in enemy:
             moves.append(ypluscheck)
-            print('enemy taken')
+            #print('enemy taken')
             break
         else:
             moves.append(ypluscheck)
-            print('appending',ypluscheck)
+            #print('appending',ypluscheck)
             continue
     yminusstart = 'start'
     while yminusstart == 'start':
         yminuscheck = yminuscheck - 8
-        print('checking yminus',yminuscheck)
+        #print('checking yminus',yminuscheck)
         if yminuscheck > 64 or yminuscheck < 1:
-            print('out of area')
+            #print('out of area')
             break
         if getPos(yminuscheck) in ally:
-            print('ally taken')
+            #print('ally taken')
             break
         elif getPos(yminuscheck) in enemy:
             moves.append(yminuscheck)
-            print('enemy taken')
+            #print('enemy taken')
             break
         else:
             moves.append(yminuscheck)
-            print('appending',yminuscheck)
+            #print('appending',yminuscheck)
             continue
     return moves
 
-characters = ['p' , 'r' , 'k' , 'b', 'q', 'k' ] #for i in OriPos , i[2]
+def bismoves(p, ally, enemy):
+    moves = []
+    xpluscheck = getPos(p)
+    xminuscheck = getPos(p)
+    ypluscheck = getPos(p)
+    yminuscheck = getPos(p)
+    #check avail spaces
+    if getrow(p) < getcolumn(p):
+        xplives = 8 - getcolumn(p)
+    else:
+        xplives = 8 - getrow(p)
+    if getrow(p) < getcolumn(p):
+        xmlives = getrow(p) - 1
+    else:
+        xmlives = getcolumn(p) - 1
+    if 8 - getrow(p) < getcolumn(p) - 1 :
+        yplives = 8 - getrow(p)
+    else:
+        yplives = getcolumn(p) - 1
+    if 8 - getcolumn(p) < getrow(p) - 1:
+        ymlives = 8 - getcolumn(p)
+    else:
+        ymlives = getrow(p) - 1
+    #print(xplives, xmlives, yplives, ymlives)
 
-#get position of piece is Pos.get(' (piece) ')
+    while True:
+        xpluscheck = xpluscheck + 9
+        #print('checking xplus',xpluscheck)
+        if getPos(xpluscheck) > 64 or getPos(xpluscheck) < 1:
+            break
+        if xplives == 0:
+            #print('off row')
+            break
+        if getPos(xpluscheck) in ally:
+            #print('ally taken')
+            break
+        elif getPos(xpluscheck) in enemy:
+            moves.append(xpluscheck)
+            #print('enemy taken')
+            break
+        else:
+            moves.append(xpluscheck)
+            xplives = xplives - 1
+            continue
+    bxminusstart = 'starting'
+    while bxminusstart == 'starting':
+        xminuscheck = xminuscheck - 9
+        #print('checking xminus',xminuscheck)
+        if getPos(xminuscheck) > 64 or getPos(xminuscheck) < 1:
+            break
+        if xmlives == 0:
+            #print('off row')
+            break
+        if getPos(xminuscheck) in ally:
+            #print('ally taken')
+            break
+        elif getPos(xminuscheck) in enemy:
+            moves.append(xminuscheck)
+            #rint('enemy taken')
+            break
+        else:
+            moves.append(xminuscheck)
+            #print('appending',xminuscheck)
+            xmlives = xmlives - 1
+            continue
+    byplusstart = 'start'
+    while byplusstart == 'start':
+        ypluscheck = ypluscheck + 7
+        #print('checking yplus',ypluscheck)
+        if ypluscheck > 64 or ypluscheck < 1:
+            #print('out of area')
+            break
+        if yplives == 0:
+            #print('diff row')
+            break
+        if getPos(ypluscheck) in ally:
+            #print('ally taken')
+            break
+        elif getPos(ypluscheck) in enemy:
+            moves.append(ypluscheck)
+            #print('enemy taken')
+            break
+        else:
+            moves.append(ypluscheck)
+            #print('appending',ypluscheck)
+            yplives = yplives - 1
+            continue
+    byminusstart = 'start'
+    while byminusstart == 'start':
+        yminuscheck = yminuscheck - 7
+        #print('checking yminus',yminuscheck)
+        if yminuscheck > 64 or yminuscheck < 1:
+            #print('out of area')
+            break
+        if ymlives == 0:
+            #print('diff row')
+            break
+        if getPos(yminuscheck) in ally:
+            #print('ally taken')
+            break
+        elif getPos(yminuscheck) in enemy:
+            moves.append(yminuscheck)
+            #print('enemy taken')
+            break
+        else:
+            moves.append(yminuscheck)
+            #print('appending',yminuscheck)
+            ymlives = ymlives -1
+            continue
+    return moves
 
+def kmoves(p, ally, enemy):
+    p = getPos(p)
+    smoves = [ p+10, p+17, p+15, p+6 , p-10, p-17, p-15, p-6]
+    lmoves = []
+    moves = []
+
+    if getcolumn(p) > 1:
+        lmoves.append(smoves[2])
+        lmoves.append(smoves[5])
+        if getcolumn(p) > 2:
+            lmoves.append(smoves[3])
+            lmoves.append(smoves[4])
+    if getcolumn(p) < 8:
+        lmoves.append(smoves[1])
+        lmoves.append(smoves[-2])
+        if getcolumn(p) < 7:
+            lmoves.append(smoves[0])
+            lmoves.append(smoves[-1])
+
+    for i in lmoves:
+        if i not in ally and i>0 and i<65:
+            moves.append(i)
+
+
+    return moves
+
+
+characters = ['p' , 'r' , 'n' , 'b', 'q', 'k' ] #for i in OriPos , i[2]
+
+#get position of piece
 def getPos(p):
     if type(p) == int:
         return p
@@ -113,10 +252,19 @@ def getPos(p):
     except:
         return int(white.get(p))
 
+#get row, column of piece
 def getrow(piece):
     pos = getPos(piece)
     return int(math.ceil(pos / 8))
+def getcolumn(piece):
+    pos = getPos(piece)
+    a = int(pos % 8)
+    if a == 0:
+        return 8
+    else:
+        return a
 
+#avail moves for pieces
 def availmoves( p ):
     moves = []
     pieceType = p[-2]
@@ -147,25 +295,69 @@ def availmoves( p ):
             for i in moves:
                 if i in takenSquares:
                     moves.remove(i)
-
     if x == 1: #rook
         if p[0] == 'b': #black rook
-
-            moves.append(rookmoves(p, btakenSquares, takenSquares))
+            for i in rookmoves(p, btakenSquares, takenSquares):
+                moves.append(i)
         else: #white rook
-            moves.append(rookmoves(p, takenSquares, btakenSquares))
+            for i in rookmoves(p, takenSquares, btakenSquares):
+                moves.append(i)
+    if x == 2: #knight
+        if p[0] == 'b':
+            for i in kmoves(p, btakenSquares, takenSquares):
+                moves.append(i)
+        else:
+            for i in kmoves(p, takenSquares, btakenSquares):
+                moves.append(i)
+    if x == 3: #bishop
+        if p[0] == 'b':
+            if p[1] == 'b': #black bishop
+                for i in bismoves(p, btakenSquares, takenSquares):
+                    moves.append(i)
+            else:
+                for i in bismoves(p, takenSquares, btakenSquares):
+                    moves.append(i)
+    if x == 4: #queen
+        if p[0] == 'b':
+#            moves.append(rookmoves(p, btakenSquares, takenSquares))
+#            moves.append(bismoves(p, btakenSquares, takenSquares))
+            r = rookmoves(p, btakenSquares, takenSquares)
+            b = bismoves(p, btakenSquares, takenSquares)
+            for i in r:
+                moves.append(i)
+            for i in b:
+                moves.append(i)
+        else:
+            r = rookmoves(p, takenSquares, btakenSquares)
+            b = bismoves(p, takenSquares, btakenSquares)
+            for i in r:
+                moves.append(i)
+            for i in b:
+                moves.append(i)
+
+    if x == 5: #king
+        if p[0] == 'b': #black king, enemy is white
+            updatethreats(bthreats, white)
+            tmoves = []
+            pos = getPos(p)
+            if getcolumn(p) >= 2:
+                tmoves.append(pos+7)
+                tmoves.append(pos-1)
+                tmoves.append(pos-9)
+            if getcolumn(p) <= 7:
+                tmoves.append(pos+9)
+                tmoves.append(pos+1)
+                tmoves.append(pos-7)
+            tmoves.append(pos+8)
+            tmoves.append(pos-8)
+            for i in tmoves:
+                if i >= 1 and i <= 64 and i not in btakenSquares and i not in bthreats:
+                    moves.append(i)
+#            print(bthreats)
 
 
-#    if x == 2: #knight
-#    if x == 3: #bishop
-#    if x == 4: #queen
-#    if x == 5: #king
+
     return moves
 
-#print('getpos',getPos('p8'))
-#print('getrow', getrow('p8'))
-print('availmoves', availmoves('r1'))
-#print('black positions:', btakenSquares)
-
-
-#print(getPos('p8'))
+print(availmoves('bk1'))
+#print(availmoves('r1'))
